e3adfbae2706ae4cf7ec7e7a1fb1b6de6cc3e3ca
phollaki <phollakos15@gmail.com>
2022-07-12 23:46:08 +0200

    Implement LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,28 @@
-import { View, Button } from 'react-native'
-import React from 'react'
+import { SafeAreaView, ImageBackground, Text, TouchableOpacity, View, Button } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import useAuth from '../hooks/useAuth'
+import { useNavigation } from '@react-navigation/native'
 
 const LoginScreen = () => {
   const {signInWithGoogle} = useAuth()
+  const navigation = useNavigation()
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerShown:false
+    })
+  },[])
+
   return (
-    <View>
-      <Button
-        title="Login"
-        onPress={() => {
-          signInWithGoogle();
-          }}
-      />
+    <View className="flex-1">
+      <ImageBackground resizeMode="cover" className="flex-1" source={{ uri:"https://tinder.com/static/tinder.png" }}>
+        <TouchableOpacity
+          onPress={() => {signInWithGoogle()}}
+          className="absolute bottom-40 w-52 bg-white rounded-full py-3"
+          style={{ marginHorizontal: "25%"}}>
+          <Text className="text-center font-semibold">Sign in & get swiping</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   )
 }
